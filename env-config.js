8144335357041f304e135a8deedf155fe01f2ad2
MOON:E <waiguogaosho@hotmail.com>
2022-07-29 19:04:25 +0900

    Update

---
+++ env-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "process.env.BACKEND_URL":
+    process.env.NODE_ENV === "production" ? "https://emoone.github.io" : "",
+};
--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "https://emoone.github.io" : "",
+};
