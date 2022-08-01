e289494670ccb1a3ae0f27117f3dab06073b4d7c
MOON:E <waiguogaosho@hotmail.com>
2022-08-01 13:43:27 +0900

    Update

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ module.exports = withPlugins(
   {
     reactStrictMode: true,
     images: {
-      domains: ["d3in0pstm1ny80.akamaized.net"],
+      domains: ["/moone-page/"],
     },
   },
   nextConfig
