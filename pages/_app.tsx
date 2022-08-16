5f597a99ddb4b23a9a80ea8e9bd012cbb30aca3e
MOON:E <waiguogaosho@hotmail.com>
2022-08-16 10:36:19 +0900

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/main.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log(process.env.NODE_ENV);
   return <Component {...pageProps} />;
 }
 
