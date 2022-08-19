c28b7c1eb0e22dbab0dbea02a273a9d6bf1abc83
MOON:E <waiguogaosho@hotmail.com>
2022-08-19 21:58:35 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/Y4BdHJDDBx_ut1ybSBqJx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-db90bb9eece03dbd.js","static/chunks/pages/index-2b2d8b73d580fc34.js"],"/_error":["static/chunks/pages/_error-c0033c2fc08817db.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
