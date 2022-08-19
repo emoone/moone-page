d597eb667dc686a4eabe94dd0faed601ac909244
MOON:E <waiguogaosho@hotmail.com>
2022-08-19 21:25:57 +0900

    Update img TEST

--- src/component/home/Home.tsx
+++ src/component/home/Home.tsx
@@ -5,6 +5,17 @@ const Home = () => {
     <TheLayout>
       <div>HomeComponent TheLayout으로 감싸기</div>
       <div>내용추가</div>
+      <div>
+        <h1>TEST IMG</h1>
+        <div className="flex wd-[100%] jutify-center">
+          <div>
+            <img src="/images/test.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="/images/test02.jpeg" alt="" />
+          </div>
+        </div>
+      </div>
     </TheLayout>
   );
 };
