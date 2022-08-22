cf446a8bfd539e4f29785598736c918205b0f87a
MOON:E <waiguogaosho@hotmail.com>
2022-08-22 09:47:54 +0900

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
 import "../styles/main.css";
 import type { AppProps } from "next/app";
+import WebFont from "webfontloader";
+import { useEffect } from "react";
+import Head from "next/head";
+// import ModalContainer from "toptoon-modal/dist/";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     WebFont.load({
+  //       google: {
+  //         families: ["Noto Sans JP:400, 500, 700", "sans-serif"],
+  //       },
+
+  //       custom: {
+  //         families: ["Noto Sans JP:400, 500, 700", "sans-serif"],
+  //         urls: ["/assets/fonts/notoSansJp/notosans-jp-origin.css"],
+  //       },
+  //     });
+  //   }
+  // }, []);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+      {/* <ModalContainer /> */}
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,91 +1,111 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import React, { useEffect } from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import WebFont from "webfontloader";
 
-const Document = () => {
-  return (
-    <Html>
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="msapplication-TileColor" content="#FF98BA" />
+          <meta name="theme-color" content="#FF98BA" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
 
-        {/* favicon area */}
-        <link
-          rel="apple-touch-icon"
-          sizes="57x57"
-          href="/favicons/apple-icon-57x57.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/favicons/apple-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/favicons/apple-icon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/favicons/apple-icon-76x76.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="/favicons/apple-icon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/favicons/apple-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/favicons/apple-icon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/favicons/apple-icon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicons/apple-icon-180x180.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/favicons/android-icon-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/favicons/favicon-96x96.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicons/favicon-16x16.png"
-        />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#ffffff"></meta>
-        {/* favicon area */}
-      </Head>
+          {/* favicon */}
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/images/assets/favicon/apple-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/images/assets/favicon/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/images/assets/favicon/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/images/assets/favicon/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/images/assets/favicon/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/images/assets/favicon/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/images/assets/favicon/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/images/assets/favicon/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/assets/favicon/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/images/assets/favicon/android-icon-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/assets/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="/images/assets/favicon/favicon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/assets/favicon/favicon-16x16.png"
+          />
+          {/* favicon */}
 
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
-export default Document;
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <link
+            href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700&display=swap"
+            rel="stylesheet"
+          />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          /> */}
+
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
