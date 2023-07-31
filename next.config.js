a82595dbbcfff328c15204925722205af2ce4b93
MOON:E <waiguogaosho@hotmail.com>
2023-07-31 21:04:46 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  output: 'export',
   experimental: {
     typedRoutes: true, // router 선언의 유효성 체크, 오타 등
+  },
+    images: {
+    loader: 'custom',
+    loaderFile: './app/image.ts',
   },
 }
  
