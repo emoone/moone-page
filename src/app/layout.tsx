f35bdc7cfa1618d51ce5aa4bbef4976e5b458412
MOON:E <waiguogaosho@hotmail.com>
2023-09-20 10:04:14 +0900

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,12 @@ export default function RootLayout({
 
   /**
    * suppressHydrationWarning관하여
-   *
+   * 서버 사이드 렌더링을 사용하는 경우, 일반적으로 서버와 클라이언트가 다른 내용을 렌더링할 때 경고가 표시됨.
+   * 그러나 매우 드물게 정확히 일치시키는 게 힘들거나 불가능합니다.
+   * 예를 들어 타임 스탬프 같은 경우 서버와 클라이언트에서는 다를 것으로 예상됩니다.
+   * suppressHydrationWarning을 true로 설정하면, React는 어트리뷰트와 그 엘리먼트 내용의 불일치에 대해 경고하지 않습니다.
+   * 바로 밑 한 단계 깊이를 기준으로만 작동하며 해결책으로 사용하도록 되어 있습니다.
+   * 남용하지 마세요. 이벤트 보충에 대한 자세한 내용은 ReactDOM.hydrateRoot() 문서를 참조해주세요.
    * 에러 메세지: Warning: Extra attributes from the server: class,style
    * 원인: 확장 UI사용시 server와 client rendering 사이에 불일치 발생하여 뜨는것으로 나타남.
    * 실제로 DOM에는 속성이 정상추가되는걸로 확인됨.
