8359211974b84f1e557dbabc59040a485adcd1a4
MOON:E <waiguogaosho@hotmail.com>
2024-04-15 09:59:19 +0900

    Update auth 기능 임시저장

--- src/components/signIn/SignInComponent.tsx
+++ src/components/signIn/SignInComponent.tsx
@@ -2,6 +2,9 @@
 
 import { SubmitHandler, useForm } from 'react-hook-form';
 
+import name from '@/app/search/page';
+import { useSession } from 'next-auth/react';
+
 interface LoginType {
   email: string;
   password: string;
@@ -9,19 +12,43 @@ interface LoginType {
 
 /**
  * ANCHOR: 로그인 페이지
+ * nextauth 기능을 사용할건데
+ * 사용방법 파악 필요(TypeScript)
+ * 1. adapter: 사용자 정의? 유형을 활용하여 구현이 예상한것과 일치하는 확인해주는건가?
+ * /** @return { import("next-auth/adapters").Adapter }
+ * function MyAdapter() {
+ *   return {
+ *      your adapter methods here
+ *   }
+ * }
+ * 2. NextAuth 모듈: 여기에 다양한 모듈 아이템이 있는듯
+ *  - session:
+ *
  */
 const SignInComponent = () => {
+  const {
+    data: session,
+    status,
+    update,
+  } = useSession({
+    required: true,
+    onUnauthenticated() {
+      //  The user is not authenticated, handle it here.
+    },
+  });
   const { register, handleSubmit } = useForm<LoginType>({
     defaultValues: { email: '', password: '' },
   });
 
   const onSubmit: SubmitHandler<LoginType> = data => {
-    console.log('data', data);
+    console.log('data', data, 'session', session, status);
+    update({ name: data.email, password: data.password });
   };
 
   return (
     <section id="loginCon">
       <h1>로그인 페이지</h1>
+      <p>signed in as {session?.user?.name}</p>
       <form action="" onSubmit={handleSubmit(onSubmit)}>
         <div className="emailCon">
           <label htmlFor="emailForm">Email: </label>
